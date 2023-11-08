1f93eb986fa0d1aed2955301dce1cac5e11a0294
WaterTravel <147826246+WaterTravel@users.noreply.github.com>
2023-11-09 00:58:15 +0900

    [Bronze V] Title: 윤년, Time: 132 ms, Memory: 9348 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.js
@@ -0,0 +1,9 @@
+  let fs = require("fs");
+      let input = fs.readFileSync("/dev/stdin").toString().split(" ");
+      let a = Number(input[0]);
+
+      if ((a % 4 === 0 && a % 100 !== 0) || a % 400 === 0) {
+        console.log(1);
+      } else {
+        console.log(0);
+      }
