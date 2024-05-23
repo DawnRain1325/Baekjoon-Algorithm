4e8b4b7ba5a51b7a90787dc91ff97b32fcb8769d
DawnRain <147826246+DawnRain1325@users.noreply.github.com>
2024-05-24 00:19:21 +0900

    Create 2739.cpp

---
+++ 2739.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+
+    std::cin >> n   ;
+    
+  
+        for (int i = 0; i < 9; i++) {
+            std::cout << n << " * " << i+1 << " = " << n * (i+1) << std::endl;
+        }
+ 
+
+    return 0;
+}
